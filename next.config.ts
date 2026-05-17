663375bccf23ec852e91e7bc9e003f7113a816e9
Lucas Aragao <lucas.damaceno@dcomp.ufs.br>
2026-05-16 23:20:06 -0300

    fix: include Prisma binary in Vercel serverless bundle

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
+  outputFileTracingIncludes: {
+    '/*': ['./src/generated/prisma/**/*'],
+  },
 }
 
 export default nextConfig
