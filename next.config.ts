b76f2a0c1b350ce1068ec468bb3c42404a028022
Lucas Aragao <lucas.damaceno@dcomp.ufs.br>
2026-05-16 23:27:02 -0300

    fix: use PrismaPlugin webpack to bundle native binary for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from 'next'
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
   outputFileTracingIncludes: {
-    '/*': ['./src/generated/prisma/**/*'],
+    '/**': ['./src/generated/prisma/**/*'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
   },
 }
 
